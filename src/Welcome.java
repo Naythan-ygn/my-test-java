18bd440f79317b5d4029d018b7d54d2846ebf602
Naythan-ygn <naythanprime@gmail.com>
2022-08-04 14:11:49 +0630

    Loop_Obj_test

---
+++ src/Welcome.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Welcome {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome Student!");
+        StudentRec str[] = new StudentRec[6];
+        for (StudentRec sr : str){
+            sr = new StudentRec();   
+        }
+
+        StudentRec sr[] = new StudentRec[6];
+        for (int i = 1; i <=5 ; i++){
+            sr[i] = new StudentRec();
+            sr[i].setName(); sr[i].setadd(); sr[i].setAge();
+            System.out.println();
+            System.out.println("Student Name: " + sr[i].getName());
+            System.out.println("Location: " + sr[i].getAdd());
+            System.out.println("Age: " + sr[i].getAge() + "\n");
+            System.out.println("---------------------------------------");
+        }
+    }
+}
+
+class StudentRec {
+    String name,add;
+    int age;
+    Scanner scan = new Scanner(System.in);
+    public void setName(){
+        System.out.print ("Enter your name : ");
+        name = scan.nextLine();
+    }
+    public void setadd(){
+        System.out.print("Enter your address : ");
+        add = scan.nextLine();
+    }
+    public void setAge(){
+        System.out.print("Enter your age : ");
+        age = scan.nextInt();
+    }
+    public String getName(){
+        return name;
+    }
+    public String getAdd(){
+        return add;
+    }
+    public int getAge(){
+        return age;
+    }
+
+}
