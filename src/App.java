c3b1319f6d6ca70586e5ce3a708ccfc742dec9c7
Naythan-ygn <naythanprime@gmail.com>
2022-08-11 23:33:31 +0630

    App_changes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+        String x = "Hello World!";
+        int i = 18;
+        double d = 113.4;
+
+        System.out.println( x+"\t" + i+"\t" + d);
     }
 }
